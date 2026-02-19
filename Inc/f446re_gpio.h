cb75a31eeb4af2708841a6f69a1e76f411ecf6f7
Bekir Akif <akifbekir2@gmail.com>
2026-02-19 16:23:55 +0200

    Modified header file for GPIO's. Added pin numbers

--- Inc/f446re_gpio.h
+++ Inc/f446re_gpio.h
@@ -31,7 +31,22 @@ typedef struct{
 #define		GPIOB				((GPIOx_typeDef*) GPIOB_PERIPH)
 #define		GPIOC				((GPIOx_typeDef*) GPIOC_PERIPH)
 
-
+//Pin numbers
+#define 	GPIO_PIN_1			1
+#define 	GPIO_PIN_2			2
+#define 	GPIO_PIN_3			3
+#define 	GPIO_PIN_4			4
+#define 	GPIO_PIN_5			5
+#define 	GPIO_PIN_6			6
+#define 	GPIO_PIN_7			7
+#define 	GPIO_PIN_8			8
+#define 	GPIO_PIN_9			9
+#define 	GPIO_PIN_10			10
+#define 	GPIO_PIN_11			11
+#define 	GPIO_PIN_12			12
+#define 	GPIO_PIN_13			13
+#define 	GPIO_PIN_14			14
+#define 	GPIO_PIN_15			15
 
 //GPIO's direction mode
 #define		INPUT_MODE			(0x0)
