fc3fe7c81bdec35fb4fa1bfb87d4823568eab1f0
Bekir Akif <akifbekir2@gmail.com>
2026-03-26 15:45:28 +0200

    Declared function for GPIO output type selection

--- Inc/gpio_driver.h
+++ Inc/gpio_driver.h
@@ -11,5 +11,6 @@ void GPIO_toogle_pin(GPIOx_typeDef* GPIOx, uint8_t pin);
 void GPIO_pull_up(GPIOx_typeDef* GPIOx, uint8_t pin);
 void GPIO_pull_up(GPIOx_typeDef* GPIOx, uint8_t pin);
 void GPIO_AF_selection_low(GPIOx_typeDef* GPIOx, uint8_t pin, uint8_t af_mode);
+void GPIO_output_type(GPIOx_typeDef* GPIOx, uint8_t pin, uint8_t o_type);
 
 #endif /* GPIO_DRIVER_H_ */
