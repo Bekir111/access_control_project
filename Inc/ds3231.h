319fccf205cc88928709be37e4d8c4a84bcfaf7e
Bekir Akif <akifbekir2@gmail.com>
2026-04-03 15:32:37 +0300

    Added header file for DS3231 module

---
+++ Inc/ds3231.h
@@ -0,0 +1,18 @@
+
+#ifndef DS3231_H_
+#define DS3231_H_
+
+#define			DS3231_ADDR			(0x68)
+
+
+typedef struct{
+	uint8_t 	day;
+	uint8_t 	month;
+	uint16_t 	year;
+	uint8_t		hour;
+	uint8_t		minute;
+}DS3231_data_t;
+
+#define BCD_TO_DEC(x)  (x - 6 * (x >> 4))
+
+#endif /* DS3231_H_ */
