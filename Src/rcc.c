60a39992fbb629a475fd283455ec6c49759f2020
Bekir Akif <akifbekir2@gmail.com>
2026-06-18 12:38:04 +0300

    Defined function to enable the peripheral clock for D peripheral

--- Src/rcc.c
+++ Src/rcc.c
@@ -5,14 +5,22 @@ void RCC_GPIOA_clock_enable(void){
 	RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOA);
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOA;
 }
+
 void RCC_GPIOB_clock_enable(void){
 	RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOB);
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOB;
 }
+
 void RCC_GPIOC_clock_enable(void){
 	RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOC);
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOC;
 }
+
+void RCC_GPIOD_clock_enable(void){
+	RCC->AHB1ENR &= ~(RCC_AHB1ENR_GPIOD);
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOD;
+}
+
 void RCC_UART2_clock_enable(void){
 	RCC->APB1ENR &= ~(RCC_APB1ENR_UART2);
 	RCC->APB1ENR |= RCC_APB1ENR_UART2;
