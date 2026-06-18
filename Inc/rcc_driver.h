4a127b325a13a00ff0a42fa1ec21b13cf118356c
Bekir Akif <akifbekir2@gmail.com>
2026-06-18 11:45:27 +0300

    Added prototypes for the functions that enables the clock for UART4 and UART5

--- Inc/rcc_driver.h
+++ Inc/rcc_driver.h
@@ -9,6 +9,8 @@ void RCC_GPIOA_clock_enable(void);
 void RCC_GPIOB_clock_enable(void);
 void RCC_GPIOC_clock_enable(void);
 void RCC_UART2_clock_enable(void);
+void RCC_UART4_clock_enable(void);
+void RCC_UART5_clock_enable(void);
 void RCC_TIM6_clock_enable(void);
 void RCC_TIM7_clock_enable(void);
 void RCC_I2C1_clock_enable(void);
